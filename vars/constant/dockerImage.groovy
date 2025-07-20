b5c22d992d9fc8e4577ccc132818e996de347c83
cyrus <cyrus.cryptorg@gmail.com>
2025-07-20 10:16:28 +0800

    Add docker registry for constants

---
+++ vars/constant/dockerImage.groovy
@@ -0,0 +1,17 @@
+class dockerRegistry {
+
+    // Docker images
+    DOCKER_JAVA_8  = "docker.io/library/java:8-debian"
+    DOCKER_JAVA_11 = "docker.io/library/java:11-debian"
+    DOCKER_JAVA_17 = "docker.io/library/java:17-debian"
+    DOCKER_JAVA_21 = "docker.io/library/java:21-debian"
+
+    // Node images
+    DOCKER_NODE_18 = "docker.io/library/node:18-debian"
+    DOCKER_NODE_21 = "docker.io/library/node:21-debian"
+
+    // yarn images
+    DOCKER_YARN_18 = "docker.io/library/yarn:18-debian"
+    DOCKER_YARN_21 = "docker.io/library/yarn:21-debian"
+
+}
